960659aeaebe73e14a1145c9eedf7020280d181c
s-ryota0810 <akaashi0810@gmail.com>
2023-10-09 23:49:07 +0900

    パスワードのハッシュ化

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { PrismaClient } = require("@prisma/client")
+const bcrypt = require("bcrypt")
 
 const PORT = 8000;
 
@@ -10,11 +11,13 @@ const prisma = new PrismaClient()
 app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await prisma.user.create({
     data: {
       username,
       email,
-      password
+      password: hashedPassword,
     }
   });
 
