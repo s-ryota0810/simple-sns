8bbc1716616765e8918b16189c34601f9c2b80c2
s-ryota0810 <akaashi0810@gmail.com>
2023-10-09 23:45:42 +0900

    新規ユーザー登録API

--- api/server.js
+++ api/server.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require("@prisma/client")
 
 const PORT = 8000;
+
+const prisma = new PrismaClient()
+
+// 新規ユーザー登録API
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password
+    }
+  });
+
+  return res.json({ user });
+})
 
 app.listen(PORT, () => {
   console.log(`server is running on Port ${PORT}`);
